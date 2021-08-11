633de38427fd8cffcee5be66ff4a206cae653799
p-komorowski <pkomorowski29@gmail.com>
2021-08-11 13:41:41 +0200

    style: better looking code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,8 @@
 import {
   Controller,
-  Request,
   Post,
-  UseGuards,
-  Get,
   Body,
 } from '@nestjs/common';
-import { JwtAuthGuard } from './strategy/jwt-auth.guard';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto';
@@ -16,18 +12,13 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('auth/register')
-  async register(@Body() req: RegisterDto) {
-    return this.authService.register(req)
+  async register(@Body() reqisterDto: RegisterDto) {
+    return this.authService.register(reqisterDto)
   }
 
   @Post('auth/login')
-  async login(@Body() req: LoginDto) {
-    return this.authService.login(req)
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto)
   }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user
-  }
 }
